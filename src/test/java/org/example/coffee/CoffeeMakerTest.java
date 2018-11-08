5f1fae03a5df9256bf5d45837b3b0b1923dc1966
rob bygrave <robin.bygrave@gmail.com>
2018-11-09 01:31:48 +1300

    Update for 0.1 generator released

--- src/test/java/org/example/coffee/CoffeeMakerTest.java
+++ src/test/java/org/example/coffee/CoffeeMakerTest.java
@@ -13,32 +13,32 @@ public class CoffeeMakerTest {
   @Test
   public void makeIt_via_SystemContext() {
 
-//    String makeIt = SystemContext.getBean(CoffeeMaker.class).makeIt();
-//    assertThat(makeIt).isEqualTo("done");
-//
-//    Pump pump = SystemContext.getBean(Pump.class);
-//    assertThat(pump).isInstanceOf(DuperPump.class);
+    String makeIt = SystemContext.getBean(CoffeeMaker.class).makeIt();
+    assertThat(makeIt).isEqualTo("done");
+
+    Pump pump = SystemContext.getBean(Pump.class);
+    assertThat(pump).isInstanceOf(DuperPump.class);
   }
 
-//  @Test
-//  public void makeIt_via_BootContext_withNoShutdownHook() {
-//
-//    try (BeanContext context = new BootContext()
-//      .withNoShutdownHook()
-//      .load()) {
-//
-//      String makeIt = context.getBean(CoffeeMaker.class) .makeIt();
-//      assertThat(makeIt).isEqualTo("done");
-//    }
-//  }
-//
-//  @Test
-//  public void makeIt_via_BootContext() {
-//
-//    try (BeanContext context = new BootContext().load()) {
-//
-//      String makeIt = context.getBean(CoffeeMaker.class) .makeIt();
-//      assertThat(makeIt).isEqualTo("done");
-//    }
-//  }
+  @Test
+  public void makeIt_via_BootContext_withNoShutdownHook() {
+
+    try (BeanContext context = new BootContext()
+      .withNoShutdownHook()
+      .load()) {
+
+      String makeIt = context.getBean(CoffeeMaker.class) .makeIt();
+      assertThat(makeIt).isEqualTo("done");
+    }
+  }
+
+  @Test
+  public void makeIt_via_BootContext() {
+
+    try (BeanContext context = new BootContext().load()) {
+
+      String makeIt = context.getBean(CoffeeMaker.class) .makeIt();
+      assertThat(makeIt).isEqualTo("done");
+    }
+  }
 }
