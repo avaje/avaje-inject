16fea7bcae4cd1fc5f07d6811df0cdd5ad8e3e3c
rob bygrave <robin.bygrave@gmail.com>
2018-11-11 16:51:12 +1300

    No effective change - add more javadoc to @Factory

--- src/main/java/io/kanuka/Factory.java
+++ src/main/java/io/kanuka/Factory.java
@@ -7,6 +7,10 @@ import java.lang.annotation.Target;
 
 /**
  * A singleton bean that has methods marked with the <code>@Bean</code> annotation.
+ * <p>
+ * Factory beans allow us to build beans using logic in methods. These methods for example
+ * often use environment variables and system properties into account when building the bean.
+ * </p>
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
