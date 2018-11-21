03cea07cc53f9af63277cb61dbfd2a539f7c83c5
rob bygrave <robin.bygrave@gmail.com>
2018-11-21 20:28:03 +1300

    #8 Re-enabled tests after we change package

--- src/test/java/org/example/coffee/BootContextAddTest.java
+++ src/test/java/org/example/coffee/BootContextAddTest.java
@@ -11,54 +11,54 @@ public class BootContextAddTest {
   @Test
   public void withModules_exludingThisOne() {
 
-//    TDPump testDoublePump = new TDPump();
-//
-//    try (BeanContext context = new BootContext()
-//      .withBeans(testDoublePump)
-//      // our module is "org.example.coffee"
-//      // so this effectively includes no modules
-//      .withModules("other")
-//      .load()) {
-//
-//      CoffeeMaker coffeeMaker = context.getBean(CoffeeMaker.class);
-//      assertThat(coffeeMaker).isNull();
-//    }
+    TDPump testDoublePump = new TDPump();
+
+    try (BeanContext context = new BootContext()
+      .withBeans(testDoublePump)
+      // our module is "org.example.coffee"
+      // so this effectively includes no modules
+      .withModules("other")
+      .load()) {
+
+      CoffeeMaker coffeeMaker = context.getBean(CoffeeMaker.class);
+      assertThat(coffeeMaker).isNull();
+    }
   }
 
 
   @Test
   public void withModules_includeThisOne() {
 
-//    TDPump testDoublePump = new TDPump();
-//
-//    try (BeanContext context = new BootContext()
-//      .withBeans(testDoublePump)
-//      .withModules("org.example.coffee")
-//      .load()) {
-//
-//      String makeIt = context.getBean(CoffeeMaker.class).makeIt();
-//      assertThat(makeIt).isEqualTo("done");
-//
-//      assertThat(testDoublePump.steam).isEqualTo(1);
-//      assertThat(testDoublePump.water).isEqualTo(1);
-//    }
+    TDPump testDoublePump = new TDPump();
+
+    try (BeanContext context = new BootContext()
+      .withBeans(testDoublePump)
+      .withModules("org.example.coffee")
+      .load()) {
+
+      String makeIt = context.getBean(CoffeeMaker.class).makeIt();
+      assertThat(makeIt).isEqualTo("done");
+
+      assertThat(testDoublePump.steam).isEqualTo(1);
+      assertThat(testDoublePump.water).isEqualTo(1);
+    }
   }
 
   @Test
   public void withBean_expect_testDoublePumpUsed() {
 
-//    TDPump testDoublePump = new TDPump();
-//
-//    try (BeanContext context = new BootContext()
-//      .withBeans(testDoublePump)
-//      .load()) {
-//
-//      String makeIt = context.getBean(CoffeeMaker.class).makeIt();
-//      assertThat(makeIt).isEqualTo("done");
-//
-//      assertThat(testDoublePump.steam).isEqualTo(1);
-//      assertThat(testDoublePump.water).isEqualTo(1);
-//    }
+    TDPump testDoublePump = new TDPump();
+
+    try (BeanContext context = new BootContext()
+      .withBeans(testDoublePump)
+      .load()) {
+
+      String makeIt = context.getBean(CoffeeMaker.class).makeIt();
+      assertThat(makeIt).isEqualTo("done");
+
+      assertThat(testDoublePump.steam).isEqualTo(1);
+      assertThat(testDoublePump.water).isEqualTo(1);
+    }
   }
 
   /**
