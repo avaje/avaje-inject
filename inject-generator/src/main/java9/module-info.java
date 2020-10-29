56f4178609cea320b2e42e786baafc84cec144d7
rob bygrave <robin.bygrave@gmail.com>
2020-10-30 00:18:31 +1300

    #53 - Tidy avaje-inject-generator module-info, remove javax.inject as brought in transitively

--- inject-generator/src/main/java9/module-info.java
+++ inject-generator/src/main/java9/module-info.java
@@ -1,9 +1,8 @@
 module io.avaje.inject.generator {
 
   requires java.compiler;
-  requires javax.inject;
-  requires java.annotation;
   requires io.avaje.inject;
+  requires java.annotation;
 
   provides javax.annotation.processing.Processor with io.avaje.inject.generator.Processor;
 }
