634b32b44079a5c99ab6af5e352f1984c855329b
rob bygrave <robin.bygrave@gmail.com>
2020-11-03 09:58:54 +1300

    Remove requires java.annotation from module-info.java
    This is no longer needed as any PostConstruct/PreDestroy can be used now using match on short name.

--- inject-generator/src/main/java9/module-info.java
+++ inject-generator/src/main/java9/module-info.java
@@ -2,7 +2,6 @@ module io.avaje.inject.generator {
 
   requires java.compiler;
   requires io.avaje.inject;
-  requires java.annotation;
 
   provides javax.annotation.processing.Processor with io.avaje.inject.generator.Processor;
 }
