ca0999e710b929d0f5390f7118f5b3b3b80c155a
rob bygrave <robin.bygrave@gmail.com>
2020-12-08 23:16:38 +1300

    Update test with jakarata package

--- inject-test/src/test/java/org/example/coffee/qualifier/Green.java
+++ inject-test/src/test/java/org/example/coffee/qualifier/Green.java
@@ -1,6 +1,6 @@
 package org.example.coffee.qualifier;
 
-import javax.inject.Qualifier;
+import jakarta.inject.Qualifier;
 import java.lang.annotation.Retention;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
