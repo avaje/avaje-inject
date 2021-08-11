88c322dd03d155fc3a596a7feba8583a6c53ee1f
rbygrave <robin.bygrave@gmail.com>
2021-08-11 16:13:18 +1200

    #144 - Implied qualifier name for constructor parameters takes [suffix] type name into account

--- inject-test/src/test/java/org/example/coffee/primary/UseNamedPEmailer.java
+++ inject-test/src/test/java/org/example/coffee/primary/UseNamedPEmailer.java
@@ -1,7 +1,7 @@
 package org.example.coffee.primary;
 
-import jakarta.inject.Named;
-import jakarta.inject.Singleton;
+import javax.inject.Named;
+import javax.inject.Singleton;
 
 @Singleton
 public class UseNamedPEmailer {
