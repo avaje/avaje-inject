806bf9c2d6381738e6738f4c1e391aa67257a4b2
Rob Bygrave <robin.bygrave@gmail.com>
2022-03-21 22:47:43 +1300

    Update for merge of java 11 to use javax.inject

--- inject/src/main/java/module-info.java
+++ inject/src/main/java/module-info.java
@@ -5,7 +5,7 @@ module io.avaje.inject {
   exports io.avaje.inject.spi;
 
   requires transitive io.avaje.lang;
-  requires transitive jakarta.inject;
+  requires transitive java.inject;
   requires static org.mockito;
 
   uses io.avaje.inject.spi.Module;
