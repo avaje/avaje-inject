ffbadfcd38db64ca7f8c088b1edf1be86f712ecb
Rob Bygrave <robin.bygrave@gmail.com>
2022-05-09 10:37:07 +1200

    #208 - Fix @InjectTest / InjectExtension to support static field injection

--- blackbox-test-inject/src/test/java/org/example/myapp/InjectExtension_viaAnnotation_Test.java
+++ blackbox-test-inject/src/test/java/org/example/myapp/InjectExtension_viaAnnotation_Test.java
@@ -2,15 +2,32 @@ package org.example.myapp;
 
 import io.avaje.inject.test.InjectTest;
 import jakarta.inject.Inject;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @InjectTest
 class InjectExtension_viaAnnotation_Test {
 
-  @Inject
-  HelloService helloService;
+  @Inject HelloService helloService;
+  static @Inject OtherService otherService;
+
+  static OtherService _tempOtherService;
+
+  @BeforeAll
+  static void beforeAll() {
+    assertThat(otherService).isNotNull();
+    _tempOtherService = otherService;
+  }
+
+  @BeforeEach
+  void beforeEach() {
+    assertThat(helloService).isNotNull();
+    assertThat(otherService).isSameAs(_tempOtherService);
+  }
 
   @Test
   void hello_1() {
--- inject-test/src/main/java/io/avaje/inject/test/InjectExtension.java
+++ inject-test/src/main/java/io/avaje/inject/test/InjectExtension.java
@@ -1,16 +1,11 @@
 package io.avaje.inject.test;
 
 import io.avaje.inject.BeanScope;
-import io.avaje.inject.BeanScopeBuilder;
-import org.junit.jupiter.api.extension.AfterEachCallback;
-import org.junit.jupiter.api.extension.BeforeAllCallback;
-import org.junit.jupiter.api.extension.BeforeEachCallback;
-import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.*;
 import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
 
 import java.lang.System.Logger.Level;
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.Method;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -18,28 +13,19 @@ import java.util.concurrent.locks.ReentrantLock;
  * <p>
  * Supports injection for fields annotated with <code>@Mock, @Spy, @Captor, @Inject</code>.
  */
-public class InjectExtension implements BeforeAllCallback, BeforeEachCallback, AfterEachCallback, ExtensionContext.Store.CloseableResource {
+public class InjectExtension implements BeforeAllCallback, AfterAllCallback, BeforeEachCallback, AfterEachCallback, ExtensionContext.Store.CloseableResource {
 
   private static final System.Logger log = System.getLogger("io.avaje.inject");
   private static final Namespace INJECT_NS = Namespace.create("io.avaje.inject.InjectTest");
   private static final String BEAN_SCOPE = "BEAN_SCOPE";
+  private static final String META = "META";
   private static final ReentrantLock lock = new ReentrantLock();
   private static boolean started;
   private static BeanScope globalTestScope;
 
-  @Override
-  public void beforeAll(ExtensionContext context) {
-    lock.lock();
-    try {
-      if (!started) {
-        initialiseGlobalTestScope(context);
-        started = true;
-      }
-    } finally {
-      lock.unlock();
-    }
-  }
-
+  /**
+   * Global shutdown of JUnit.
+   */
   @Override
   public void close() {
     lock.lock();
@@ -61,51 +47,95 @@ public class InjectExtension implements BeforeAllCallback, BeforeEachCallback, A
     }
   }
 
+  @Override
+  public void beforeAll(ExtensionContext context) {
+    lock.lock();
+    try {
+      if (!started) {
+        initialiseGlobalTestScope(context);
+        started = true;
+      }
+
+      final MetaInfo metaInfo = createMetaInfo(context);
+      putMetaInfo(context, metaInfo);
+
+      if (metaInfo.hasStaticInjection()) {
+        BeanScope testClassBeanScope = metaInfo.buildForClass(globalTestScope);
+        putClassScope(context, testClassBeanScope);
+      }
+    } finally {
+      lock.unlock();
+    }
+  }
+
+  private void putMetaInfo(ExtensionContext context, MetaInfo metaInfo) {
+    Class<?> testClass = context.getRequiredTestClass();
+    context.getStore(INJECT_NS).put(META + testClass, metaInfo);
+  }
+
+  private MetaInfo getMetaInfo(ExtensionContext context) {
+    Class<?> testClass = context.getRequiredTestClass();
+    return (MetaInfo) context.getStore(INJECT_NS).get(META + testClass);
+  }
+
+  private void putClassScope(ExtensionContext context, BeanScope testClassBeanScope) {
+    Class<?> testClass = context.getRequiredTestClass();
+    context.getStore(INJECT_NS).put(BEAN_SCOPE + testClass, testClassBeanScope);
+  }
+
+  private BeanScope getClassScope(ExtensionContext context) {
+    Class<?> testClass = context.getRequiredTestClass();
+    return (BeanScope) context.getStore(INJECT_NS).get(BEAN_SCOPE + testClass);
+  }
+
   /**
    * Callback that is invoked <em>before</em> each test is invoked.
    */
   @Override
   public void beforeEach(final ExtensionContext context) {
-    final List<MetaReader> readers = createMetaReaders(context);
-    final BeanScopeBuilder builder = BeanScope.builder();
-    if (globalTestScope != null) {
-      builder.parent(globalTestScope, false);
-    }
-    // register mocks and spies local to this test
-    for (MetaReader reader : readers) {
-      reader.build(builder);
-    }
+    final MetaInfo metaInfo = getMetaInfo(context);
+    if (metaInfo.hasNormalInjection()) {
 
-    // wire with local mocks, spies, and globalTestScope
-    final BeanScope beanScope = builder.build();
-    for (MetaReader reader : readers) {
-      reader.setFromScope(beanScope);
+      // if (static fields) then (class scope) else (globalTestScope)
+      final BeanScope parent = metaInfo.hasStaticInjection() ? getClassScope(context) : globalTestScope;
+
+      BeanScope beanScope = metaInfo.buildForInstance(parent, context.getRequiredTestInstance());
+
+      // put method level test scope
+      Method testMethod = context.getRequiredTestMethod();
+      context.getStore(INJECT_NS).put(BEAN_SCOPE + testMethod, beanScope);
     }
-    log.log(Level.TRACE, "test setup with {0}", readers);
-    context.getStore(INJECT_NS).put(BEAN_SCOPE, beanScope);
   }
 
   /**
-   * Return the list of MetaReaders - 1 per test instance.
-   */
-  private List<MetaReader> createMetaReaders(ExtensionContext context) {
-    final List<Object> testInstances = context.getRequiredTestInstances().getAllInstances();
-    final List<MetaReader> readers = new ArrayList<>(testInstances.size());
-    for (Object testInstance : testInstances) {
-      readers.add(new MetaReader(testInstance));
-    }
-    return readers;
-  }
-
-  /**
-   * Callback that is invoked <em>after</em> each test has been invoked.
+   * Cleanup scope for test method.
    */
   @Override
   public void afterEach(ExtensionContext context) {
-    final BeanScope beanScope = (BeanScope) context.getStore(INJECT_NS).remove(BEAN_SCOPE);
+    Method testMethod = context.getRequiredTestMethod();
+    final BeanScope beanScope = (BeanScope) context.getStore(INJECT_NS).remove(BEAN_SCOPE + testMethod);
     if (beanScope != null) {
       beanScope.close();
     }
+  }
+
+  /**
+   * Cleanup scope for class (static fields).
+   */
+  @Override
+  public void afterAll(ExtensionContext context) {
+    Class<?> testClass = context.getRequiredTestClass();
+    final BeanScope testClassBeanScope = (BeanScope) context.getStore(INJECT_NS).remove(BEAN_SCOPE + testClass);
+    if (testClassBeanScope != null) {
+      testClassBeanScope.close();
+    }
+  }
+
+  /**
+   * Return the MetaInfo.
+   */
+  private MetaInfo createMetaInfo(ExtensionContext context) {
+    return new MetaInfo(context.getRequiredTestClass());
   }
 
 }
