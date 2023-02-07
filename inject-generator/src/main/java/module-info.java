ad3f269c4b992550e552d8b3db2c7f05c76b90b9
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-02-07 18:57:29 -0500

    static inject

--- inject-generator/src/main/java/module-info.java
+++ inject-generator/src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module io.avaje.inject.generator {
 
   requires java.compiler;
-  requires io.avaje.inject;
-  requires hickory;
+  requires static io.avaje.inject;
+  requires static hickory;
 
   uses io.avaje.inject.spi.Plugin;
   uses io.avaje.inject.spi.Module;
