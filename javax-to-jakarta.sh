a95ca9ad36ad82459de8ccca896038556d772261
Rob Bygrave <robin.bygrave@gmail.com>
2023-02-23 20:12:42 +1300

    Add scripts to perform Jakarta to Javax conversion (jakarta-to-javax.sh)  (#293)
    * Jakarta to Javax via scripts (jakarta-to-javax.sh)
    
    With the view of maintaining the javax 'fork' via scripted
    conversion of jakarta code rather than maintaining
     the javax.master branch.

---
+++ javax-to-jakarta.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+## adjust pom dependencies
+sed -i'' -e 's|<version>1\.0\.5</version> <!-- javax -->|<version>2\.0\.1</version> <!-- jakarta -->|g' inject/pom.xml
+
+## adjust module-info
+sed -i'' -e 's| java\.inject| jakarta\.inject|g' inject/src/main/java/module-info.java
+
+## adjust code
+find . -type f -name '*.java' -exec sed -i'' -e 's| javax\.inject\.| jakarta\.inject\.|g' {} +
