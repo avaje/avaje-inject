d26cd731d2a8004e0405843dd64d9c5604c714eb
rob <robin.bygrave@gmail.com>
2023-02-23 20:09:44 +1300

    Jakarta to Javax via scripts - tighten up

--- jakarta-to-javax.sh
+++ jakarta-to-javax.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 ## adjust pom dependencies
-sed -i'' -e 's|<version>2.0.1</version> <!-- jakarta -->|<version>1.0.5</version> <!-- javax -->|g' inject/pom.xml
+sed -i'' -e 's|<version>2\.0\.1</version> <!-- jakarta -->|<version>1\.0\.5</version> <!-- javax -->|g' inject/pom.xml
 
 ## adjust module-info
-sed -i'' -e 's| jakarta.inject;| java.inject;|g' inject/src/main/java/module-info.java
+sed -i'' -e 's| jakarta\.inject| java\.inject|g' inject/src/main/java/module-info.java
 
 ## adjust code
-find . -type f -name '*.java' -exec sed -i'' -e 's|jakarta\.inject\.|javax\.inject\.|g' {} +
+find . -type f -name '*.java' -exec sed -i'' -e 's| jakarta\.inject\.| javax\.inject\.|g' {} +
