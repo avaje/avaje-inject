7fedecd35af8daf6a40ce8791a9082a785e166be
rob <robin.bygrave@gmail.com>
2023-02-23 16:58:08 +1300

    Jakarta to Javax via scripts - opps

--- jakarta-to-javax.sh
+++ jakarta-to-javax.sh
@@ -7,4 +7,4 @@ sed -i'' -e 's|<version>2.0.1</version> <!-- jakarta -->|<version>1.0.5</version
 sed -i'' -e 's| jakarta.inject;| java.inject;|g' inject/src/main/java/module-info.java
 
 ## adjust code
-find . -type f -name '*.java' -exec sed -i'' -e 's|javax.inject.|jakarta.inject.|g' {} +
+find . -type f -name '*.java' -exec sed -i'' -e 's|jakarta\.inject\.|javax\.inject\.|g' {} +
