6d72f91a906649fb9b49739519773ab43fc53d21
Rob Bygrave <robin.bygrave@gmail.com>
2023-03-01 19:39:25 +1300

    Fix jakarta-to-javax.sh script

--- jakarta-to-javax.sh
+++ jakarta-to-javax.sh
@@ -7,4 +7,5 @@ sed -i'' -e 's|<version>2\.0\.1</version> <!-- jakarta -->|<version>1\.0\.5</ver
 sed -i'' -e 's| jakarta\.inject| java\.inject|g' inject/src/main/java/module-info.java
 
 ## adjust code
-find . -type f -name '*.java' -exec sed -i'' -e 's| jakarta\.inject\.| javax\.inject\.|g' {} +
+#find . -type f -name '*.java' -exec sed -i'' -e 's| jakarta\.inject\.| javax\.inject\.|g' {} +
+find . -type f -not -name 'IncludeAnnotations.java' -name '*.java' -exec sed -i'' -e 's|jakarta\.inject\.|javax\.inject\.|g' {} +
