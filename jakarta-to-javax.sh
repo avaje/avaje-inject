ff92103209d3f16c2bf96cd603e0f329db927c1b
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-03-10 17:48:44 -0500

    Even more specialized script

--- jakarta-to-javax.sh
+++ jakarta-to-javax.sh
@@ -2,13 +2,13 @@
 
 ## adjust pom dependencies
 
-sed -i 's/<version>\(.*\)-SNAPSHOT<\/version>/<version>\1-javax-SNAPSHOT<\/version>/' pom.xml
-sed -i 's/<version>\(.*\)-SNAPSHOT<\/version>/<version>\1-javax-SNAPSHOT<\/version>/' inject/pom.xml
-sed -i 's/<version>\(.*\)-SNAPSHOT<\/version>/<version>\1-javax-SNAPSHOT<\/version>/' inject-generator/pom.xml
-sed -i 's/<version>\(.*\)-SNAPSHOT<\/version>/<version>\1-javax-SNAPSHOT<\/version>/' inject-test/pom.xml
-sed -i 's/<version>\(.*\)-SNAPSHOT<\/version>/<version>\1-javax-SNAPSHOT<\/version>/' blackbox-aspect/pom.xml
-sed -i 's/<version>\(.*\)-SNAPSHOT<\/version>/<version>\1-javax-SNAPSHOT<\/version>/' blackbox-other/pom.xml
-sed -i 's/<version>\(.*\)-SNAPSHOT<\/version>/<version>\1-javax-SNAPSHOT<\/version>/' blackbox-test-inject/pom.xml
+sed -i '0,/<version>[^<]*\(-SNAPSHOT\)\{0,1\}<\/version>/ s/<version>\([^<]*\)\(-SNAPSHOT\)\{0,1\}<\/version>/<version>\1-javax\2<\/version>/' pom.xml
+sed -i '0,/<version>[^<]*\(-SNAPSHOT\)\{0,1\}<\/version>/ s/<version>\([^<]*\)\(-SNAPSHOT\)\{0,1\}<\/version>/<version>\1-javax\2<\/version>/' inject/pom.xml
+sed -i '0,/<version>[^<]*\(-SNAPSHOT\)\{0,1\}<\/version>/ s/<version>\([^<]*\)\(-SNAPSHOT\)\{0,1\}<\/version>/<version>\1-javax\2<\/version>/' inject-generator/pom.xml
+sed -i '0,/<version>[^<]*\(-SNAPSHOT\)\{0,1\}<\/version>/ s/<version>\([^<]*\)\(-SNAPSHOT\)\{0,1\}<\/version>/<version>\1-javax\2<\/version>/' inject-test/pom.xml
+sed -i '0,/<version>[^<]*\(-SNAPSHOT\)\{0,1\}<\/version>/ s/<version>\([^<]*\)\(-SNAPSHOT\)\{0,1\}<\/version>/<version>\1-javax\2<\/version>/' blackbox-aspect/pom.xml
+sed -i '0,/<version>[^<]*\(-SNAPSHOT\)\{0,1\}<\/version>/ s/<version>\([^<]*\)\(-SNAPSHOT\)\{0,1\}<\/version>/<version>\1-javax\2<\/version>/' blackbox-other/pom.xml
+sed -i '0,/<version>[^<]*\(-SNAPSHOT\)\{0,1\}<\/version>/ s/<version>\([^<]*\)\(-SNAPSHOT\)\{0,1\}<\/version>/<version>\1-javax\2<\/version>/' blackbox-test-inject/pom.xml
 
 sed -i'' -e 's|<version>2\.0\.1</version> <!-- jakarta -->|<version>1\.0\.5</version> <!-- javax -->|g' inject/pom.xml
 
