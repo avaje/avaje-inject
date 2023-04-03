18a0dbf3a18ed9569f3f7239c80a26e2e89015eb
Rob Bygrave <robin.bygrave@gmail.com>
2023-04-04 09:01:29 +1200

    Make javax-to-jakarta sed compatible with MacOS

--- jakarta-to-javax.sh
+++ jakarta-to-javax.sh
@@ -10,11 +10,11 @@ sed -i -E '0,/<version>[^<]*<\/version>/ s/<version>([^-]*)-?([^<]*)(<\/version>
 sed -i -E '0,/<version>[^<]*<\/version>/ s/<version>([^-]*)-?([^<]*)(<\/version>)/<version>\1-javax-\2\3/' blackbox-other/pom.xml
 sed -i -E '0,/<version>[^<]*<\/version>/ s/<version>([^-]*)-?([^<]*)(<\/version>)/<version>\1-javax-\2\3/' blackbox-test-inject/pom.xml
 
-sed -i'' -e 's|<version>2\.0\.1</version> <!-- jakarta -->|<version>1\.0\.5</version> <!-- javax -->|g' inject/pom.xml
+sed -i '' -e 's|<version>2\.0\.1</version> <!-- jakarta -->|<version>1\.0\.5</version> <!-- javax -->|g' inject/pom.xml
 
 ## adjust module-info
-sed -i'' -e 's| jakarta\.inject| java\.inject|g' inject/src/main/java/module-info.java
+sed -i '' -e 's| jakarta\.inject| java\.inject|g' inject/src/main/java/module-info.java
 
 ## adjust code
 #find . -type f -name '*.java' -exec sed -i'' -e 's| jakarta\.inject\.| javax\.inject\.|g' {} +
-find . -type f -not -name 'IncludeAnnotations.java' -name '*.java' -exec sed -i'' -e 's|jakarta\.inject\.|javax\.inject\.|g' {} +
+find . -type f -not -name 'IncludeAnnotations.java' -name '*.java' -exec sed -i '' -e 's|jakarta\.inject\.|javax\.inject\.|g' {} +
--- javax-to-jakarta.sh
+++ javax-to-jakarta.sh
@@ -9,10 +9,10 @@ sed -i 's/\(<version>[^<]*\)-javax\([^<]*<\/version>\)/\1\2/' inject-test/pom.xm
 sed -i 's/\(<version>[^<]*\)-javax\([^<]*<\/version>\)/\1\2/' blackbox-aspect/pom.xml
 sed -i 's/\(<version>[^<]*\)-javax\([^<]*<\/version>\)/\1\2/' blackbox-other/pom.xml
 sed -i 's/\(<version>[^<]*\)-javax\([^<]*<\/version>\)/\1\2/' blackbox-test-inject/pom.xml
-sed -i'' -e 's|<version>1\.0\.5</version> <!-- javax -->|<version>2\.0\.1</version> <!-- jakarta -->|g' inject/pom.xml
+sed -i '' -e 's|<version>1\.0\.5</version> <!-- javax -->|<version>2\.0\.1</version> <!-- jakarta -->|g' inject/pom.xml
 
 ## adjust module-info
-sed -i'' -e 's| java\.inject| jakarta\.inject|g' inject/src/main/java/module-info.java
+sed -i '' -e 's| java\.inject| jakarta\.inject|g' inject/src/main/java/module-info.java
 
 ## adjust code
-find . -type f -not -name 'IncludeAnnotations.java' -name '*.java' -exec sed -i'' -e 's|javax\.inject\.|jakarta\.inject\.|g' {} +
+find . -type f -not -name 'IncludeAnnotations.java' -name '*.java' -exec sed -i '' -e 's|javax\.inject\.|jakarta\.inject\.|g' {} +
