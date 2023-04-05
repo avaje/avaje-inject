fdf0be2bcc5df1d5750e518c1ef47fd3150f7bc2
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-04-05 10:00:29 -0400

    fix javax scripts to work on both mac and linux

--- javax-to-jakarta.sh
+++ javax-to-jakarta.sh
@@ -16,3 +16,13 @@ sed -i '' -e 's| java\.inject| jakarta\.inject|g' inject/src/main/java/module-in
 
 ## adjust code
 find . -type f -not -name 'IncludeAnnotations.java' -name '*.java' -exec sed -i '' -e 's|javax\.inject\.|jakarta\.inject\.|g' {} +
+
+## Linux version
+
+sed -i'' -e 's|<version>1\.0\.5</version> <!-- javax -->|<version>2\.0\.1</version> <!-- jakarta -->|g' inject/pom.xml
+
+## adjust module-info
+sed -i'' -e 's| java\.inject| jakarta\.inject|g' inject/src/main/java/module-info.java
+
+## adjust code
+find . -type f -not -name 'IncludeAnnotations.java' -name '*.java' -exec sed -i'' -e 's|javax\.inject\.|jakarta\.inject\.|g' {} +
