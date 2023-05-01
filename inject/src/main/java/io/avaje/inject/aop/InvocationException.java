f3a42045f14bcae14ca893b3a21967923299c6df
Rob Bygrave <robin.bygrave@gmail.com>
2023-05-01 19:13:20 +1200

    Format InvocationException only

--- inject/src/main/java/io/avaje/inject/aop/InvocationException.java
+++ inject/src/main/java/io/avaje/inject/aop/InvocationException.java
@@ -2,23 +2,29 @@ package io.avaje.inject.aop;
 
 /**
  * Exception occurring during method interception.
- *
- * <p>When using aspects and {@link MethodInterceptor} any throwable that is undeclared on the
+ * <p>
+ * When using aspects and {@link MethodInterceptor} any throwable that is undeclared on the
  * method is caught and re-throw as an InvocationException.
  */
 public class InvocationException extends RuntimeException {
 
-  /** Create with a given cause. */
+  /**
+   * Create with a given cause.
+   */
   public InvocationException(String message) {
     super(message);
   }
 
-  /** Create with a given cause. */
+  /**
+   * Create with a given cause.
+   */
   public InvocationException(Throwable cause) {
     super(cause);
   }
 
-  /** Create with a given message and cause. */
+  /**
+   * Create with a given message and cause.
+   */
   public InvocationException(String message, Throwable cause) {
     super(message, cause);
   }
