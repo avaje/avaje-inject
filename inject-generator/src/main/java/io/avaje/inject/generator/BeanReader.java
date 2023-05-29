c7bcab8531f07205d3e554cbd5a5b438418e709a
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-05-29 15:17:14 -0500

    Sanitize DI class imports (#351)
    * sanitize imports
    
    * test

--- inject-generator/src/main/java/io/avaje/inject/generator/BeanReader.java
+++ inject-generator/src/main/java/io/avaje/inject/generator/BeanReader.java
@@ -303,7 +303,7 @@ final class BeanReader {
     }
     for (String importType : importTypes()) {
       if (Util.validImportType(importType)) {
-        writer.append("import %s;", importType).eol();
+        writer.append("import %s;", Util.sanitizeimports(importType)).eol();
       }
     }
     writer.eol();
