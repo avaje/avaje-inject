a81abb11640b71973f7b079898eb2d95b25af999
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-07-03 17:44:20 -0400

    Update DSystemProps.java

--- inject/src/main/java/io/avaje/inject/DSystemProps.java
+++ inject/src/main/java/io/avaje/inject/DSystemProps.java
@@ -14,19 +14,9 @@ final class DSystemProps implements io.avaje.inject.spi.PropertyRequiresPlugin {
   public boolean contains(String property) {
     return System.getProperty(property) != null || System.getenv(property) != null;
   }
-
-  @Override
-  public boolean missing(String property) {
-    return System.getProperty(property) == null && System.getenv(property) == null;
-  }
-
+  
   @Override
   public boolean equalTo(String property, String value) {
     return value.equals(System.getProperty(property)) || value.equals(System.getenv(property));
-  }
-
-  @Override
-  public boolean notEqualTo(String property, String value) {
-    return !value.equals(System.getProperty(property)) && !value.equals(System.getenv(property));
   }
 }
