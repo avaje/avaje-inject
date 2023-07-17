fc9be4b0e0bee38b40e5164d013570c6e3d3f21f
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-07-16 23:36:39 -0400

    info log modules if property set

--- inject/src/main/java/io/avaje/inject/DBeanScopeBuilder.java
+++ inject/src/main/java/io/avaje/inject/DBeanScopeBuilder.java
@@ -12,7 +12,7 @@ import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-import static java.lang.System.Logger.Level.DEBUG;
+import static java.lang.System.Logger.Level.*;
 
 /**
  * Build a bean scope with options for shutdown hook and supplying test doubles.
@@ -236,7 +236,11 @@ final class DBeanScopeBuilder implements BeanScopeBuilder.ForTesting {
         " Review IntelliJ Settings / Build / Build tools / Gradle - 'Build and run using' value and set that to 'Gradle'. " +
         " Refer to https://avaje.io/inject#gradle");
     }
-    log.log(DEBUG, "building with modules {0}", moduleNames);
+    
+    log.log(
+        propertyRequiresPlugin.contains("printModules") ? INFO : DEBUG,
+        "building with modules {0}",
+        moduleNames);
 
     final Builder builder = Builder.newBuilder(propertyRequiresPlugin, suppliedBeans, enrichBeans, parent, parentOverride);
     for (final Module factory : factoryOrder.factories()) {
