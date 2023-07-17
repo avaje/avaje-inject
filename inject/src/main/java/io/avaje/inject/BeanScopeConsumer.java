1289e3c63a7dab19b1a7d9af6727f517425e3829
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-07-17 19:20:50 -0400

    Update BeanScopeConsumer.java

--- inject/src/main/java/io/avaje/inject/BeanScopeConsumer.java
+++ inject/src/main/java/io/avaje/inject/BeanScopeConsumer.java
@@ -1,6 +1,6 @@
 package io.avaje.inject;
 
 import java.util.function.Consumer;
-/** Consumer interface that takes a completed BeanScope as an argument */
+/** Consumer interface that takes a fully wired BeanScope as an argument */
 @FunctionalInterface
 public interface BeanScopeConsumer extends Consumer<BeanScope> {}
