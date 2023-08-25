c73aa9f1b64e27ac1300aecb2fa4686a4fb0c53a
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-08-25 19:00:20 -0400

    test

---
+++ blackbox-aspect/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module blackbox.aspect {
+
+  requires io.avaje.inject;
+
+  //remove this and compilation fails
+  provides io.avaje.inject.spi.Module with org.example.external.aspect.sub.ExampleExternalAspectModule;
+}
