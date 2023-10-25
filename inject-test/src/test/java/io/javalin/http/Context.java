6ac673ff1b3ad9bb96a888e61acc136a31c001ea
Rob Bygrave <robin.bygrave@gmail.com>
2023-10-25 23:24:53 +1300

    Use placeholder for Javalin Context with RequestScope test

---
+++ inject-test/src/test/java/io/javalin/http/Context.java
@@ -0,0 +1,8 @@
+package io.javalin.http;
+
+/**
+ * Placeholder for Javalin Context. Odd issue with a transitive dependency
+ * on 5.6.0 specifically org.jetbrains:annotations:24.0.1
+ */
+public class Context {
+}
