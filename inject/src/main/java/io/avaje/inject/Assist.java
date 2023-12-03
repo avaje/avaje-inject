975dbb13a12e739a58795d2f915b7390b1468097
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-12-02 20:04:33 -0500

    will fix docs later

--- inject/src/main/java/io/avaje/inject/Assist.java
+++ inject/src/main/java/io/avaje/inject/Assist.java
@@ -7,9 +7,7 @@ import static java.lang.annotation.RetentionPolicy.SOURCE;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-/**
- * @see InjectModule#ignoreSingleton()
- */
+/** */
 @Target({FIELD, PARAMETER})
 @Retention(SOURCE)
 public @interface Assist {}
--- inject/src/main/java/io/avaje/inject/Assisted.java
+++ inject/src/main/java/io/avaje/inject/Assisted.java
@@ -11,42 +11,7 @@ import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.CLASS;
 
 /**
- * Identify a bean as component with singleton scope that avaje-inject will use.
- * <p>
- * This is an alternative to using the standard <code>@Singleton</code> annotation.
- * We generally use <code>@Component</code> when we:
- * <ul>
- *   <li>Want to use avaje-inject in a project that has some other library using <code>@Singleton</code></li>
- *   <li>Want to support BOTH <code>javax.inject</code> and <code>jakarta.inject</code></li>
- * </ul>
  *
- * <h3>Example</h3>
- * <pre>{@code
- *
- * @Component
- * class MyEmailSender implements EmailSender {
- *
- *   ...
- * }
- * }</pre>
- *
- * <h3>Ignoring <em>@Singleton</em></h3>
- * <p>
- * Set {@link InjectModule#ignoreSingleton()} <code>true</code> to get avaje-inject to ignore
- * classes annotated with <code>@Singleton</code>. Typically, we want another DI library to use
- * those classes and want avaje-inject to co-exist independently.
- * <p>
- *
- * <pre>{@code
- *
- *   @InjectModule(name = "coffee", ignoreSingleton = true)
- *   package coffee;
- *
- *   import io.avaje.inject.InjectModule;
- *
- * }</pre>
- *
- * @see InjectModule#ignoreSingleton()
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
