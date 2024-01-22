5f4f7a5c1ac4c9d1481fab4e927be61aec4808ed
Rob Bygrave <robin.bygrave@gmail.com>
2024-01-22 23:35:39 +1300

    Add test for when AssistedFactory has no target factory
    Noting that the generated AssistedFactory can't be injected
    into other components at compile time because it doesn't
    exist in the first compilation round

---
+++ blackbox-test-inject/src/main/java/org/example/myapp/assist/OtherThing.java
@@ -0,0 +1,19 @@
+package org.example.myapp.assist;
+
+import io.avaje.inject.Component;
+//import org.example.myapp.assist.css.OtherScanner$AssistFactory;
+
+@Component
+public class OtherThing {
+
+//  private final OtherScanner$AssistFactory factory;
+//
+//  OtherThing(OtherScanner$AssistFactory factory) {
+//    this.factory = factory;
+//  }
+//
+//  public String scan(String file) {
+//    Scanner scanner = factory.create(file);
+//    return scanner.scan();
+//  }
+}
--- blackbox-test-inject/src/test/java/org/example/myapp/assist/css/CssFactoryTest.java
+++ blackbox-test-inject/src/test/java/org/example/myapp/assist/css/CssFactoryTest.java
@@ -2,6 +2,7 @@ package org.example.myapp.assist.css;
 
 import io.avaje.inject.BeanScope;
 import org.example.myapp.assist.CssFactory;
+import org.example.myapp.assist.CssThing;
 import org.example.myapp.assist.Scanner;
 import org.junit.jupiter.api.Test;
 
@@ -19,6 +20,16 @@ class CssFactoryTest {
 
       String result = scanner.scan();
       assertThat(result).isEqualTo("scanWith|path=one | somethin=hi");
+
+      var cssThing = testScope.get(CssThing.class);
+      String two = cssThing.scan("two");
+      assertThat(two).isEqualTo("scanWith|path=two | somethin=hi");
+
+      var otherAssistFactory = testScope.get(OtherScanner$AssistFactory.class);
+      assertThat(otherAssistFactory).isNotNull();
+
+      // String other = otherThing.scan("other");
+      // assertThat(other).isEqualTo("scanWith|path=two | somethin=hi");
     }
   }
 
