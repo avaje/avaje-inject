7d8402ffdd0d6aecdc69af151aa9f146142293e3
Rob Bygrave <robin.bygrave@gmail.com>
2024-01-24 23:00:56 +1300

    Update javadoc of AssistFactory to reflect that parameter names need to match

--- inject/src/main/java/io/avaje/inject/AssistFactory.java
+++ inject/src/main/java/io/avaje/inject/AssistFactory.java
@@ -15,30 +15,37 @@ import java.lang.annotation.Target;
  *
  * <h3>Example</h3>
  * <p>
- * Have an interface which we want avaje-inject to generate the implementation.
+ * Have an interface which we want avaje-inject to generate an implementation.
  * We desire this because the factory has some dependencies that are components
  * that avaje-inject should inject for us and some other dependencies that are
  * provided by the application (and annotated with {@code Assisted}).
+ * <p>
+ * The factory interface must only have 1 method defined.
+ *
  * <pre>{@code
  *
  * public interface CssFactory {
  *
- *   Scanner scanner(Path path);
+ *   Scanner scanner(Path myPath);
  * }
  *
  * }</pre>
  * <p>
  * Have a bean annotated with {@code @AssistFactory} that specifies the factory.
- * Any dependencies that are annotated with {@code Assist} will be parameters
+ * Any dependencies that are annotated with {@code Assisted} will be parameters
  * of the factory method, the other dependencies are normal components that
  * will be injected.
+ * <p>
+ * The {@code Assisted} parameters must match the factory method parameters
+ * by name and type. In this example {@code Path myPath} match in both CssScanner
+ * and in CssFactory.
  *
  * <pre>{@code
  *
  * @AssistFactory(CssFactory.class)
  * class CssScanner implements Scanner {
  *
- *   private final Path path;
+ *   private final Path myPath;
  *   private final SomeComponent someComponent;
  *
  *   CssScanner(@Assisted Path path, SomeComponent someComponent) {
