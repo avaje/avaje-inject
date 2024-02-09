f5bc7036f2b167b668aacae106a79f34b2d042b1
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-02-09 02:37:17 -0500

    test default methods

--- inject-generator/src/test/java/io/avaje/inject/generator/models/valid/assist/CarFactory.java
+++ inject-generator/src/test/java/io/avaje/inject/generator/models/valid/assist/CarFactory.java
@@ -2,6 +2,9 @@ package io.avaje.inject.generator.models.valid.assist;
 
 import java.util.List;
 
-public interface CarFactory {
-  Car construct(Paint paint, int size, List<String> type);
+public abstract class CarFactory {
+
+  public abstract Car construct(Paint paint, int size, List<String> type);
+
+  public void nonFactory() {}
 }
