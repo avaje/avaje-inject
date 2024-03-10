cc7fc1ea597f67895009b3374085926a054c0a8e
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-03-10 17:58:02 -0400

    tests

---
+++ inject-test/src/test/java/org/example/coffee/provider/FactoryProviderTest.java
@@ -0,0 +1,23 @@
+package org.example.coffee.provider;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.function.Supplier;
+
+import org.junit.jupiter.api.Test;
+
+import io.avaje.inject.BeanScope;
+import io.avaje.inject.spi.GenericType;
+import jakarta.inject.Provider;
+
+class FactoryProviderTest {
+
+  @Test
+  void test() {
+    var scope = BeanScope.builder().build();
+
+    Supplier<String> prov = BeanScope.builder().build().get(new GenericType<Supplier<String>>() {});
+    assertThat(prov.get()).isEqualTo("Stand proud Provider, you were strong");
+    assertThat(scope.get(String.class, "second")).isEqualTo("Nah, I'd win");
+  }
+}
