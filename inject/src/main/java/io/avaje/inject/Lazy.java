f0424588672b7775ce670aed71fbb3d88cce81d8
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-03-12 15:39:01 -0400

    Update Lazy.java

--- inject/src/main/java/io/avaje/inject/Lazy.java
+++ inject/src/main/java/io/avaje/inject/Lazy.java
@@ -6,10 +6,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Marks a Singleton, Component or Factory method beans to be initialised lazily.
+ * Marks a Singleton, Component or Factory method beans to be initialized lazily.
  * <p>
  * When annotating a {@link Factory} as {@code @Lazy} it means that the factory
- * itself is not lazy but all beans that it provides will have lazy initialisation.
+ * itself is not lazy but all beans that it provides will have lazy initialization.
  */
 @Retention(RetentionPolicy.SOURCE)
 @Target({ElementType.METHOD, ElementType.TYPE})
