a7cbf5220e7c30bb9ce8a1a1dd68817d35601820
Rob Bygrave <robin.bygrave@gmail.com>
2024-05-02 21:02:31 +1200

    Prefer intra-module beans when resolving type (v2) (#557)
    Based on #549 but move current module into DBeanMap

--- inject/src/test/java/io/avaje/inject/spi/DContextEntryTest.java
+++ inject/src/test/java/io/avaje/inject/spi/DContextEntryTest.java
@@ -3,8 +3,7 @@ package io.avaje.inject.spi;
 import io.avaje.inject.BeanEntry;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SuppressWarnings("all")
 class DContextEntryTest {
@@ -13,22 +12,22 @@ class DContextEntryTest {
   void get_when_onePrimary() {
 
     DContextEntry entry = new DContextEntry();
-    entry.add(DContextEntryBean.of("P", null, BeanEntry.PRIMARY));
-    entry.add(DContextEntryBean.of("N", null, BeanEntry.NORMAL));
-    entry.add(DContextEntryBean.of("S", null, BeanEntry.SECONDARY));
+    entry.add(DContextEntryBean.of("P", null, BeanEntry.PRIMARY, null));
+    entry.add(DContextEntryBean.of("N", null, BeanEntry.NORMAL, null));
+    entry.add(DContextEntryBean.of("S", null, BeanEntry.SECONDARY, null));
 
-    assertEquals(entry.get(null), "P");
+    assertEquals(entry.get(null, null), "P");
   }
 
   @Test
   void get_when_twoPrimary() {
     assertThrows(IllegalStateException.class, () -> {
       DContextEntry entry = new DContextEntry();
-      entry.add(DContextEntryBean.of("P", null, BeanEntry.PRIMARY));
-      entry.add(DContextEntryBean.of("N", null, BeanEntry.NORMAL));
-      entry.add(DContextEntryBean.of("S", null, BeanEntry.PRIMARY));
+      entry.add(DContextEntryBean.of("P", null, BeanEntry.PRIMARY, null));
+      entry.add(DContextEntryBean.of("N", null, BeanEntry.NORMAL, null));
+      entry.add(DContextEntryBean.of("S", null, BeanEntry.PRIMARY, null));
 
-      entry.get(null);
+      entry.get(null, null);
     });
   }
 
@@ -36,10 +35,10 @@ class DContextEntryTest {
   void get_when_oneNormal() {
 
     DContextEntry entry = new DContextEntry();
-    entry.add(DContextEntryBean.of("N", null, BeanEntry.NORMAL));
-    entry.add(DContextEntryBean.of("S", null, BeanEntry.SECONDARY));
+    entry.add(DContextEntryBean.of("N", null, BeanEntry.NORMAL, null));
+    entry.add(DContextEntryBean.of("S", null, BeanEntry.SECONDARY, null));
 
-    assertEquals(entry.get(null), "N");
+    assertEquals(entry.get(null, null), "N");
   }
 
 
@@ -47,11 +46,11 @@ class DContextEntryTest {
   void get_when_oneNormal2() {
 
     DContextEntry entry = new DContextEntry();
-    entry.add(DContextEntryBean.of("N", null, BeanEntry.NORMAL));
-    entry.add(DContextEntryBean.of("S1", null, BeanEntry.SECONDARY));
-    entry.add(DContextEntryBean.of("S2", null, BeanEntry.SECONDARY));
+    entry.add(DContextEntryBean.of("N", null, BeanEntry.NORMAL, null));
+    entry.add(DContextEntryBean.of("S1", null, BeanEntry.SECONDARY, null));
+    entry.add(DContextEntryBean.of("S2", null, BeanEntry.SECONDARY, null));
 
-    assertEquals(entry.get(null), "N");
+    assertEquals(entry.get(null, null), "N");
   }
 
   @Test
@@ -59,10 +58,10 @@ class DContextEntryTest {
 
     assertThrows(IllegalStateException.class, () -> {
       DContextEntry entry = new DContextEntry();
-      entry.add(DContextEntryBean.of("S1", null, BeanEntry.SECONDARY));
-      entry.add(DContextEntryBean.of("S2", null, BeanEntry.SECONDARY));
+      entry.add(DContextEntryBean.of("S1", null, BeanEntry.SECONDARY, null));
+      entry.add(DContextEntryBean.of("S2", null, BeanEntry.SECONDARY, null));
 
-      entry.get(null);
+      entry.get(null, null);
     });
   }
 
@@ -70,29 +69,121 @@ class DContextEntryTest {
   void get_when_multiSecondary_butNamed() {
 
     DContextEntry entry = new DContextEntry();
-    entry.add(DContextEntryBean.of("S1", "a", BeanEntry.SECONDARY));
-    entry.add(DContextEntryBean.of("S2", "b", BeanEntry.SECONDARY));
+    entry.add(DContextEntryBean.of("S1", "a", BeanEntry.SECONDARY, null));
+    entry.add(DContextEntryBean.of("S2", "b", BeanEntry.SECONDARY, null));
 
-    assertEquals(entry.get("b"), "S2");
+    assertEquals(entry.get("b", null), "S2");
   }
 
   @Test
   void get_when_multiSecondary_butNamed2() {
 
     DContextEntry entry = new DContextEntry();
-    entry.add(DContextEntryBean.of("S1", null, BeanEntry.SECONDARY));
-    entry.add(DContextEntryBean.of("S2", "b", BeanEntry.SECONDARY));
+    entry.add(DContextEntryBean.of("S1", null, BeanEntry.SECONDARY, null));
+    entry.add(DContextEntryBean.of("S2", "b", BeanEntry.SECONDARY, null));
 
-    assertEquals(entry.get("b"), "S2");
+    assertEquals(entry.get("b", null), "S2");
   }
 
   @Test
   void get_when_secondary_butNamed() {
 
     DContextEntry entry = new DContextEntry();
-    entry.add(DContextEntryBean.of("S1", null, BeanEntry.PRIMARY));
-    entry.add(DContextEntryBean.of("S2", "b", BeanEntry.SECONDARY));
+    entry.add(DContextEntryBean.of("S1", null, BeanEntry.PRIMARY, null));
+    entry.add(DContextEntryBean.of("S2", "b", BeanEntry.SECONDARY, null));
 
-    assertEquals(entry.get("b"), "S2");
+    assertEquals(entry.get("b", null), "S2");
+  }
+
+  @Test
+  void getWithName_when_currentModule() {
+
+    DContextEntry entry = new DContextEntry();
+    entry.add(DContextEntryBean.of("N1", "same", BeanEntry.NORMAL, Module.class));
+    entry.add(DContextEntryBean.of("N2", "same", BeanEntry.NORMAL, null));
+    entry.add(DContextEntryBean.of("N3", "same", BeanEntry.NORMAL, OtherModule.class));
+
+    assertEquals(entry.get("same", null), "N2");
+    assertEquals(entry.get("same", Module.class), "N1");
+    assertEquals(entry.get("same", OtherModule.class), "N3");
+
+    assertEquals(entry.get(null, null), "N2");
+    assertEquals(entry.get(null, Module.class), "N1");
+    assertEquals(entry.get(null, OtherModule.class), "N3");
+  }
+
+  @Test
+  void getWithoutName_when_currentModule() {
+
+    DContextEntry entry = new DContextEntry();
+    entry.add(DContextEntryBean.of("N1", null, BeanEntry.NORMAL, Module.class));
+    entry.add(DContextEntryBean.of("N2", null, BeanEntry.NORMAL, null));
+    entry.add(DContextEntryBean.of("N3", null, BeanEntry.NORMAL, OtherModule.class));
+
+    assertEquals(entry.get(null, null), "N2");
+    assertEquals(entry.get(null, Module.class), "N1");
+    assertEquals(entry.get(null, OtherModule.class), "N3");
+
+    assertNull(entry.get("notThere", null));
+    assertNull(entry.get("notThere", Module.class));
+    assertNull(entry.get("notThere", OtherModule.class));
+  }
+
+  @Test
+  void getWithName_expect_matchOnName() {
+
+    DContextEntry entry = new DContextEntry();
+    entry.add(DContextEntryBean.of("N1", "same", BeanEntry.NORMAL, Module.class));
+    entry.add(DContextEntryBean.of("N2", null, BeanEntry.NORMAL, null));
+    entry.add(DContextEntryBean.of("N3", null, BeanEntry.NORMAL, OtherModule.class));
+
+    assertEquals(entry.get("same", null), "N1");
+    assertEquals(entry.get("same", Module.class), "N1");
+    assertEquals(entry.get("same", OtherModule.class), "N1");
+  }
+
+  @Test
+  void getWithoutName_expect_matchOnNoName() {
+
+    DContextEntry entry = new DContextEntry();
+    entry.add(DContextEntryBean.of("N1", null, BeanEntry.NORMAL, Module.class));
+    entry.add(DContextEntryBean.of("N2", "same", BeanEntry.NORMAL, null));
+    entry.add(DContextEntryBean.of("N3", "same", BeanEntry.NORMAL, OtherModule.class));
+
+    assertEquals(entry.get(null, null), "N1");
+    assertEquals(entry.get(null, Module.class), "N1");
+    assertEquals(entry.get(null, OtherModule.class), "N1");
+  }
+
+  @Test
+  void when_noQualifier_expect_matchedToNoQualider() {
+
+    DContextEntry entry = new DContextEntry();
+    entry.add(DContextEntryBean.of("N1", null, BeanEntry.NORMAL, Module.class));
+    entry.add(DContextEntryBean.of("N2", "myName", BeanEntry.NORMAL, Module.class));
+
+    assertEquals(entry.get("myName", Module.class), "N2");
+    assertEquals(entry.get(null, Module.class), "N1");
+  }
+
+  @Test
+  void when_noQualifier_expect_matchedToNoQualider2() {
+
+    DContextEntry entry = new DContextEntry();
+    entry.add(DContextEntryBean.of("N1", null, BeanEntry.NORMAL, null));
+    entry.add(DContextEntryBean.of("N2", "myName", BeanEntry.NORMAL, null));
+
+    assertEquals(entry.get("myName", Module.class), "N2");
+    assertEquals(entry.get(null, Module.class), "N1");
+  }
+
+  class OtherModule implements Module {
+    @Override
+    public Class<?>[] classes() {
+      return new Class[0];
+    }
+    @Override
+    public void build(Builder builder) {
+    }
   }
 }
