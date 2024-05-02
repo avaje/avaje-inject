bc0e1155916c8c6e282427a5e094349a168460ea
Rob Bygrave <robin.bygrave@gmail.com>
2024-05-02 21:39:34 +1200

    Javadoc improvement for @External

--- inject/src/main/java/io/avaje/inject/External.java
+++ inject/src/main/java/io/avaje/inject/External.java
@@ -7,10 +7,14 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+import java.lang.reflect.Type;
 
 /**
- * Marks this dependency as an external bean not managed by avaje inject. Compile-time validation
- * will be disabled for this type.
+ * Marks this dependency as an external bean not managed by avaje inject.
+ * Compile-time validation will be disabled for this type.
+ * <p>
+ * The external dependency is expected to be provided by
+ * {@link BeanScopeBuilder#bean(String, Type, Object)}.
  */
 @Documented
 @Retention(RUNTIME)
