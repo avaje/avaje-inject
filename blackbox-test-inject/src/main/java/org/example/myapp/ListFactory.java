f42ff710a58dfd73e749d1e267b6759464a64057
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-05-28 00:11:17 -0400

    fix tests

--- blackbox-test-inject/src/main/java/org/example/myapp/ListFactory.java
+++ blackbox-test-inject/src/main/java/org/example/myapp/ListFactory.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import io.avaje.inject.Bean;
 import io.avaje.inject.Factory;
+import jakarta.inject.Named;
 
 @Factory
 public class ListFactory {
 
   @Bean
+  @Named("listStrings")
   List<String> test() {
     return List.of("test1", "test2");
   }
