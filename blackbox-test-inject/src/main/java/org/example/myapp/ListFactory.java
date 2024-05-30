164e8f181053364e2990e36a67a31f371cab680c
Rob Bygrave <robin.bygrave@gmail.com>
2024-05-30 20:46:04 +1200

    Add test for multi @Bean with other lifecycle options

--- blackbox-test-inject/src/main/java/org/example/myapp/ListFactory.java
+++ blackbox-test-inject/src/main/java/org/example/myapp/ListFactory.java
@@ -5,6 +5,7 @@ import java.util.List;
 import io.avaje.inject.Bean;
 import io.avaje.inject.Factory;
 import io.avaje.inject.PostConstruct;
+import io.avaje.inject.PreDestroy;
 
 @Factory
 public class ListFactory {
@@ -24,14 +25,40 @@ public class ListFactory {
     return List.of(new Some("m1"), new Some("m2"), new Some("m3"));
   }
 
-  @Bean
+  @Bean(initMethod = "initMe")
+  List<Some> multiple2() {
+    return List.of(new Some("mx1"), new Some("mx2"));
+  }
+
+  @Bean(initMethod = "postInit")
   Some one() {
     return new Some("s1");
+  }
+
+  @Bean
+  List<Some2> multipleVanilla() {
+    return List.of(new Some2("v1"), new Some2("v2"));
+  }
+
+  @Bean
+  Some2 oneVanilla() {
+    return new Some2("v3");
+  }
+
+  @Bean
+  List<Some3> multiple3() {
+    return List.of(new Some3("31"), new Some3("32"));
+  }
+
+  @Bean
+  List<Some4> multiple4() {
+    return List.of(new Some4("41"), new Some4("42"));
   }
 
   public static class Some {
 
     public final String name;
+    public String other;
 
     public Some(String name) {
       this.name = name;
@@ -41,9 +68,68 @@ public class ListFactory {
       return name;
     }
 
+    public String other() {
+      return other;
+    }
+
+    public void initMe() {
+      other = name + "initMe";
+    }
+
     @PostConstruct
+    public void postInit() {
+      other = name + "postInit";
+    }
+
+    @PreDestroy
+    void shutdown() {
+      other = "stop";
+    }
+  }
+
+  public static class Some2 {
+
+    public final String name;
+
+    public Some2(String name) {
+      this.name = name;
+    }
+
+    public String name() {
+      return name;
+    }
+  }
+
+  public static class Some3 {
+
+    public final String name;
+
+    public Some3(String name) {
+      this.name = name;
+    }
+
+    public String name() {
+      return name;
+    }
+    @PreDestroy
     void shutdown() {
 
+    }
+  }
+
+  public static class Some4 {
+
+    public final String name;
+
+    public Some4(String name) {
+      this.name = name;
+    }
+
+    public String name() {
+      return name;
+    }
+    @PostConstruct
+    void myInitMethod() {
     }
   }
 }
