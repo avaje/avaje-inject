24f50a291c4edb345abdf7f3985575dac908f214
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-06-09 23:17:31 -0400

    split aop into a separate module

--- blackbox-aspect/src/main/java/module-info.java
+++ blackbox-aspect/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module blackbox.aspect {
 
   requires io.avaje.inject;
+  requires io.avaje.inject.aop;
 
   //remove this and compilation fails
   provides io.avaje.inject.spi.InjectSPI with org.example.external.aspect.sub.ExampleExternalAspectModule;
--- inject-generator/src/main/java/module-info.java
+++ inject-generator/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module io.avaje.inject.generator {
 
   requires java.compiler;
   requires io.avaje.inject;
+  requires io.avaje.inject.aop;
   requires static io.avaje.prism;
 
   uses io.avaje.inject.spi.InjectSPI;
