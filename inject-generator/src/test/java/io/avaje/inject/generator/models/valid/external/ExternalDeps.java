15bbce6157176285e6273057af39feacbc780fe7
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-07-18 08:24:08 -0400

    Update ExternalDeps.java

--- inject-generator/src/test/java/io/avaje/inject/generator/models/valid/external/ExternalDeps.java
+++ inject-generator/src/test/java/io/avaje/inject/generator/models/valid/external/ExternalDeps.java
@@ -7,12 +7,12 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.jspecify.annotations.Nullable;
 
 import io.avaje.inject.External;
+import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
 @Singleton
 public class ExternalDeps {
-
-  @Nullable AtomicLong longyAtomicInteger;
+  @Inject @Nullable AtomicLong longy;
 
   public ExternalDeps(@External AtomicBoolean bool, @Nullable AtomicInteger inty) {}
 }
