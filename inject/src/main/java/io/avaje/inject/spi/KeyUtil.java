e4af7bf7c17ef05b2e8d8b7d4b547900b3e33180
Rob Bygrave <robin.bygrave@gmail.com>
2024-08-29 00:27:19 +1200

    Add support for building against Valhalla EA

--- inject/src/main/java/io/avaje/inject/spi/KeyUtil.java
+++ inject/src/main/java/io/avaje/inject/spi/KeyUtil.java
@@ -5,8 +5,7 @@ import java.lang.reflect.Type;
 /**
  * Build a "flat key" combining type and name.
  */
-final class KeyUtil {
-
+final /*value*/ class KeyUtil {
 
   static String key(Type type, String name) {
     return name == null ? type.getTypeName() : type.getTypeName() + "|" + name.toLowerCase();
