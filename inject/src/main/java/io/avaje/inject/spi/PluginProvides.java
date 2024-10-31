f6efe62b480845ae665eae3581678b35fdd684a9
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-10-31 12:40:45 -0400

    Update PluginProvides.java

--- inject/src/main/java/io/avaje/inject/spi/PluginProvides.java
+++ inject/src/main/java/io/avaje/inject/spi/PluginProvides.java
@@ -5,15 +5,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/** Hold bean dependency metadata for compile time InjectPlugin auto detection. */
+/** Registers {@link InjectPlugin} classes for auto detection */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.CLASS)
 public @interface PluginProvides {
 
-  /** The types this component provides. */
+  /** The types this plugin provides. */
   Class<?>[] provides() default {};
 
-  /** Fully Qualified Strings of the classes provided. Use when the plugin provides generic types */
+  /** Fully Qualified Strings of the classes provided. Use when providing generic types */
   String[] providesStrings() default {};
 
   /** The aspects this component provides. */
