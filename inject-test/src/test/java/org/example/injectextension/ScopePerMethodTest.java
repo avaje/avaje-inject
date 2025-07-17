14704989a392b42f15b3aa068b00d730b047aeda
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-07-17 16:38:10 -0400

    [test] add a switch to create a scope per test (#859)

---
+++ inject-test/src/test/java/org/example/injectextension/ScopePerMethodTest.java
@@ -0,0 +1,26 @@
+package org.example.injectextension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import io.avaje.inject.test.InjectTest;
+import jakarta.inject.Inject;
+
+@InjectTest(scopePerMethod = true)
+class ScopePerMethodTest {
+  @Inject ƎNA ena;
+  static ƎNA ayna;
+
+  @Test
+  void one() {
+    assertThat(ena).isNotSameAs(ayna);
+    ayna = ena;
+  }
+
+  @Test
+  void two() {
+    assertThat(ena).isNotSameAs(ayna);
+    ayna = ena;
+  }
+}
---
+++ inject-test/src/test/java/org/example/injectextension/ƎNA.java
@@ -0,0 +1,13 @@
+package org.example.injectextension;
+
+import jakarta.inject.Singleton;
+
+@Singleton
+public class ƎNA {
+
+  public String greet() {
+    return "Ah, these are the most utmost grand of days";
+  }
+
+  public void giveGift() {}
+}
