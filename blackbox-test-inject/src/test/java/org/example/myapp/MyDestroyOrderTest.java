e52e26d9e3eb439f07b3a5ca3a0f7905c04a90cd
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-08-08 22:05:09 -0400

    Reverse PreDestroy Order (#883)
    * Reverse PreDestroy Order
    
    Now will execute same priority predestroy hooks in reverse order

--- blackbox-test-inject/src/test/java/org/example/myapp/MyDestroyOrderTest.java
+++ blackbox-test-inject/src/test/java/org/example/myapp/MyDestroyOrderTest.java
@@ -3,19 +3,35 @@ package org.example.myapp;
 import io.avaje.inject.BeanScope;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MyDestroyOrderTest {
 
   @Test
-  void ordering() {
+  void ordering_byPriority() {
     MyDestroyOrder.ordering().clear();
     try (BeanScope beanScope = BeanScope.builder().build()) {
       beanScope.get(HelloService.class);
     }
-    List<String> ordering = MyDestroyOrder.ordering();
-    assertThat(ordering).containsExactly("HelloService", "AppConfig", "MyNamed", "MyMetaDataRepo", "ExampleService", "AppHelloData");
+    assertThat(MyDestroyOrder.ordering())
+      .containsExactly(
+        "HelloService",
+        "MyNamed",
+        "AppConfig",
+        "MyMetaDataRepo",
+        "ExampleService",
+        "AppHelloData");
+  }
+
+  @Test
+  void ordering_expect_reverseOfDependencies() {
+    MyDestroyOrder2.ordering().clear();
+    try (BeanScope beanScope = BeanScope.builder().build()) {
+      beanScope.get(HelloService.class);
+    }
+    assertThat(MyDestroyOrder2.ordering())
+      .containsExactly(
+        "Two",
+        "One");
   }
 }
