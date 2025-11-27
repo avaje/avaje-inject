c8b32026bc5bd54b401d33a8ee6297b8bd9fc2b8
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-11-27 01:44:48 -0500

    Fix Lazy Priority Generation
    It seems that the `asPrimary` code was not getting generated

--- inject-generator/src/test/java/io/avaje/inject/generator/models/valid/lazy/LazyBean.java
+++ inject-generator/src/test/java/io/avaje/inject/generator/models/valid/lazy/LazyBean.java
@@ -3,12 +3,14 @@ package io.avaje.inject.generator.models.valid.lazy;
 import java.io.IOException;
 
 import io.avaje.inject.Lazy;
+import io.avaje.inject.Primary;
 import jakarta.inject.Inject;
 import jakarta.inject.Provider;
 import jakarta.inject.Singleton;
 
 @Lazy
 @Singleton
+@Primary
 public class LazyBean {
 
   Provider<Integer> intProvider;
